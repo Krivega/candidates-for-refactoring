4b46f6b2b9e8fa33a323142e8d8ef6a0976c9961
ravilbashirov <41636124+ravilbashirov@users.noreply.github.com>
2024-09-23 19:55:35 +0300

    Update SomeClass2.ts

--- src/SomeClass2.ts
+++ src/SomeClass2.ts
@@ -1,10 +1,10 @@
 class SomeClass2 {
-  private unusedVariable: number;
+  // private unusedVariable: number;
 
-  constructor() {
-    console.log('Constructor is unnecessary if not initializing anything');
-  }
-
+  // constructor() {
+  //   console.log('Constructor is unnecessary if not initializing anything');
+  // }
+//судя по всему этот метод лишний, неиспользуемый - ценности нет, только байты памяти занимает
   public someMethod(): void {
     console.log('This method performs a useful action');
   }
